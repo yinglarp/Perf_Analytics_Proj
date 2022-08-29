cacf5d08c080a01bf30e8f94d00bde5dac258636
YL Leong <yinglarp@gmail.com>
2022-08-29 22:48:33 +0800

    Added logic for perf metrics

--- src/perf_metrics.py
+++ src/perf_metrics.py
@@ -7,8 +7,11 @@ class Metrics:
     def __init__(self):
         pass
     
-    def calculate_return_series(series: pd.Series) -> pd.Series:
+    def compute_cumulative_return(series: pd.Series) -> pd.Series:
         """
+        -function annotation to indicate the func has a return type of Series data struct
+        -function annotation to indicate that the func takes in a series obj as its parameters
+
         Calculates the return series of a given time series.
 
         >>> data = load_eod_data('VBB')
@@ -19,3 +22,12 @@ class Metrics:
         """
         shifted_series = series.shift(1, axis=0)
         return series / shifted_series - 1
+
+    def compute_return_percentage(series: pd.Series) -> float:
+        """
+        takes the first and last value in a return series and compute the return and converting the result to percentage
+        assumes the return series is sorted in ascending order by dates
+        """
+        return (series.iloc[-1] / series.iloc[0] - 1) * 100
+        
+
