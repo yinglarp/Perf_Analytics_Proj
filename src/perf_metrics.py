88761353bf48e581d076231f449501a1e9efcff8
YL Leong <yinglarp@gmail.com>
2022-08-31 00:06:40 +0800

    Added main.py to test  perf_metrics

--- src/perf_metrics.py
+++ src/perf_metrics.py
@@ -3,12 +3,12 @@ import numpy as np
 import pandas as pd
 
 
-class Metrics:
+class PerfMetrics:
 
     def __init__(self):
         pass
     
-    def compute_daily_return(self, df: pd.DataFrame) -> pd.DataFrame:
+    def compute_daily_return(self, rtn_series: pd.Series) -> pd.Series:
         """
         -function annotation to indicate the func has a return type of Series data struct
         -function annotation to indicate that the func takes in a series obj as its parameters
@@ -22,36 +22,44 @@ class Metrics:
         The first value will always be NaN.
         Drop all NAN values using drop method -> df.dropna(inplace = True)
         """
-        shifted_df = df.shift(1, axis=0)
-        return (df / shifted_df).dropna(inplace=True) - 1
+        shifted_rtn_series = rtn_series.shift(1, axis=0)
+        return (rtn_series / shifted_rtn_series).dropna(inplace=True) - 1
 
-    def compute_return_percentage(self, df: pd.DataFrame) -> float:
+    def compute_return_percentage(self, rtn_series: pd.Series) -> float:
         """
         >>> takes the first and last value in a return series and compute the return and converting the result to percentage
         >>> assumes the return series is sorted in ascending order by dates
         """
-        return (df.iloc[-1] / df.iloc[0] - 1) * 100
+        return (rtn_series.iloc[-1] / rtn_series.iloc[0] - 1) * 100
     
-    def compute_annualization_factor(self, df: pd.DataFrame, days_in_year: float) -> float:
+    def compute_annualization_factor(self, rtn_series: pd.Series, days_in_year: float) -> float:
         """
         >>> calculate the years within the period of analysis based on the date index of the dataframe obj for annualization
         >>> assumes the return series is indexed by date
         >>> days_in_year = 365.25
         """
-        analysis_period_start_date = df.index[0]
-        analysis_period_end_date = df.index[-1]
+        analysis_period_start_date = rtn_series.index[0]
+        analysis_period_end_date = rtn_series.index[-1]
         annualization_factor = (analysis_period_end_date - analysis_period_start_date).days / days_in_year
         return annualization_factor
 
-    def compute_annualized_return(self, df: pd.DataFrame) -> float:
+    def compute_annualized_return(self, price_series: pd.Series) -> float:
         """
         >>> calculate annualized return 
         """
-        start_price = df.iloc[0]
-        end_price = df.iloc[-1]
+        start_price = price_series.iloc[0]
+        end_price = price_series.iloc[-1]
         pror_usd = end_price / start_price
-        annul_factor = self.compute_annualization_factor(df)
+        annul_factor = self.compute_annualization_factor(price_series)
         return (pror_usd ** (1 / annul_factor)) - 1
+
+    def compute_cumulative_return(self, rtn_series:pd.Series) -> float:
+        """
+        >>> Input: return series with datetimeindex 
+        >>> Return: chainlinked return series
+        """
+        return (rtn_series + 1).prod() - 1
+
 
 
 
