0edf7f10d46436d7c1ab59427f2e38bb97e2d2d3
YL Leong <yinglarp@gmail.com>
2022-08-31 00:26:44 +0800

    added class instance to trigger funcs

--- scripts/main.py
+++ scripts/main.py
@@ -1,12 +1,15 @@
 import numpy as np
 import pandas as pd
 from datetime import datetime
-import src.perf_metrics as pm
+from src.perf_metrics import PerfMetrics
 
 dates = [datetime(2022,8,1),datetime(2022,8,2),datetime(2022,8,3),datetime(2022,8,4),datetime(2022,8,5),datetime(2022,8,6),datetime(2022,8,7),datetime(2022,8,8)]
 rtn_series = pd.Series(np.random.randn(8), index=dates)
 print(rtn_series)
 print(rtn_series.index)
-print(rtn_series.prod())
-print(pm.PerfMetrics.compute_cumulative_return(rtn_series))
+print((rtn_series+1).prod()-1)
+
+#create an instance of the class PerfMetrics
+pm = PerfMetrics()
+print(pm.compute_cumulative_return(rtn_series))
 
